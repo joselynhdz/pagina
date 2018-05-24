845a4c47c53a1dda3cb70ccf3a5a189ecf8475b4
Joselyn Hernandez <hernandezcruzjoselyn38@gmail.com>
2018-05-23 19:43:03 -0500

    dde

--- scripts/app.js
+++ scripts/app.js
@@ -10,10 +10,10 @@ var click = document.querySelector('.click');
 var links = document.querySelectorAll(".photos a");
 links.forEach(link => link.addEventListener("click",function(e){
     e.preventDefault();
-    //asignar a una variable la img de click
+  
     var img = this.querySelector('img');
     var viewimg= click.querySelector('img');
     viewimg.src = img.src;
-    //asignar el src de arriba a la variable img de click
+    
     click.style.display="flex";
 }));
