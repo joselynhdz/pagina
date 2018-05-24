f62235fb75c43a1624752f5c57b92850827bed1e
Joselyn Hernandez <hernandezcruzjoselyn38@gmail.com>
2018-05-23 19:37:18 -0500

    terminado

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,19 @@
-/*
- * 3) Dentro de la función "handleSubmit" sucederá todo el proceso
- * de evaluación, usamos e.preventDefault() para evitar que el
- * formulario intente enviar los datos a otro lado como está
- * programado por definición.
- * Para recabar cada uno de los valores de los inputs utilizamos 
- * líneas como la del ejemplo: 
- * e.target.<nombre_del_input>.value
- */
-function handleSubmit(e){
-  e.preventDefault();
-  var diasTrabajo = e.target.diasdetrabajo.value;
-  var horasDia = e.target.horasaldia.value;
-  var horasTrabajo = diasTrabajo * horasDia * 52;
-  var horasdenotrabajar = diaslibres * horasdia + diasdeinactividad * horasdias;
-  var tiempoadministrativo = porcentajedejuntas * (horastrabajo - horasdeinactividad);
-  var gastos = gastosmensuales * 12;
-  var precioextr = horasdeinactividad + tiempoadministrativo + gastosfijos;
-  var horasefectivas = (horasposibles - horasdeinactividad - tiempoadministrativo) * horasdia;
-  var horas = horasdia + (horadia * rentabilidad) + (horasdia * jubilacion);
-  
-  
-  
+var cerrar = document.querySelector('.click');
+var click = document.querySelector('.click');
 
-}
+  cerrar.addEventListener("click",function(e){
+    e.preventDefault();
+    click.style.display="none";
 
+})
 
-/*
- * 2) Lo que hacemos dentro de la función "initialize" es tomar 
- * el formulario del DOM y asignarlo a una variable para después
- * pasar nuestra función "handleSubmit" como listener del evento
- * submit de este formulario.
- */
-function initialize(){
-  const form = document.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
-}
-
-/* 
- * 1) Al cargar la página manda llamar
- * la función "initialize"
- */
-window.onload = (function(){
-  initialize();
-})();
+var links = document.querySelectorAll(".photos a");
+links.forEach(link => link.addEventListener("click",function(e){
+    e.preventDefault();
+    //asignar a una variable la img de view
+    var img = this.querySelector('img');
+    var viewimg= click.querySelector('img');
+    viewimg.src = img.src;
+    //asignar el src de arriba a la variable img de view
+    click.style.display="flex";
+}));
